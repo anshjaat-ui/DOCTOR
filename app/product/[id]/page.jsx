0dcf01478fd861d59fbb775e6c2f37a1401ece00
anshjaat-ui <anshwalyan@gmail.com>
2026-07-21 08:04:23 -0700

    Delete app/product/[id] directory

--- app/product/[id]/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function ProductPage({ params }) {
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1>Product ID: {params.id}</h1>
-      <p>This is product detail page</p>
-    </div>
-  );
-}
