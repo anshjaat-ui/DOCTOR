ced362fd7b6c5e90581a00997f0eff7053745f80
anshjaat-ui <anshwalyan@gmail.com>
2026-07-21 07:07:10 -0700

    Delete medical-e-commerce-storefront-development directory

--- medical-e-commerce-storefront-development/src/app/checkout/page.tsx
+++
@@ -1,6 +0,0 @@
-import { CheckoutForm } from "@/components/checkout-form";
-import { SiteFooter, SiteHeader } from "@/components/site-shell";
-
-export default function CheckoutPage() {
-  return <main className="min-h-screen bg-[#fffdfa] text-[#27362c]"><SiteHeader /><CheckoutForm /><SiteFooter /></main>;
-}
